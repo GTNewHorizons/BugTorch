9b3441aa6221a421ffe443ec44a7eef5023e68cd
jss2a98aj <jss2a98aj@gmail.com>
2023-05-06 18:02:13 -0400

    Fix some vanilla block sounds

--- src/main/java/jss/bugtorch/modsupport/VanillaSupport.java
+++ src/main/java/jss/bugtorch/modsupport/VanillaSupport.java
@@ -15,6 +15,21 @@ public class VanillaSupport {
 		}
 
 		//Bugfixes
+		if(BugTorchConfig.fixBlockSounds) {
+			Blocks.bed.setStepSound(Block.soundTypeCloth);
+			Blocks.tripwire.setStepSound(Block.soundTypeCloth);
+			Blocks.web.setStepSound(Block.soundTypeCloth);
+
+			Blocks.redstone_wire.setStepSound(Block.soundTypeSand);
+
+			Blocks.jukebox.setStepSound(Block.soundTypeWood);
+			Blocks.noteblock.setStepSound(Block.soundTypeWood);
+			Blocks.tripwire_hook.setStepSound(Block.soundTypeWood);
+
+			Blocks.heavy_weighted_pressure_plate.setStepSound(Block.soundTypeMetal);
+			Blocks.light_weighted_pressure_plate.setStepSound(Block.soundTypeMetal);
+		}
+
 		if(BugTorchConfig.fixJackOLanternBlocksRandomlyTicking) {
 			Blocks.lit_pumpkin.setTickRandomly(false);
 		}
